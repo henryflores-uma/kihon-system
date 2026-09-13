b0993db470c9fa95ac8a6c3da8f7c1318a56fa2c
Henry Flores <henryfloresestrada11@gmail.com>
2026-09-13 00:51:07 -0500

    index.html completo

--- src/main/java/com/kihon/kihon/security/SecurityConfig.java
+++ src/main/java/com/kihon/kihon/security/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
 
                                                 // Frontend público
                                                 .requestMatchers(
+                                                                "/",
                                                                 "/login",
                                                                 "/dashboard",
                                                                 "/estudiantes",
